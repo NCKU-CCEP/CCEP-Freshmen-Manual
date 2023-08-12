08c6e6f68f28f73505a14d7fb606812cfdcef2d7
Aka2210 <qoo0973215128@gmail.com>
2023-08-12 19:59:01 +0800

    Added opening guide function in mobile phone

--- FreshmenManual/src/script.js
+++ FreshmenManual/src/script.js
@@ -2,4 +2,21 @@ import * as MyFunction from './function.js';
 
 $(document).ready(() => {
     MyFunction.setClick()
+
+    $(".OPENGuide").on('click', () => {
+        if($(".DataPageGuide").css('display') === 'none')
+        {
+            $(".DataPageGuide").css('display', 'block');
+            $(".OPENGuide").text('(Close)>>');
+            $(".OPENGuide").css("left","26.5%");
+        }
+        else
+        {
+            $(".DataPageGuide").css('display', 'none');
+            $(".OPENGuide").text('<<(Guide)');
+            $(".OPENGuide").css("left","3%");
+        }
+
+        
+    })
 })
